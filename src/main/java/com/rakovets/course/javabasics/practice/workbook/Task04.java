5567416c754127fdd6102845348e6b0b5f59e88e
Dzmitry <dimazm@live.com>
2020-10-02 10:52:11 +0300

    Add files via upload

---
+++ src/main/java/com/rakovets/course/javabasics/practice/workbook/Task04.java
@@ -0,0 +1,16 @@
+package com.rakovets.course.javabasics.practice.workbook;
+
+public class Task04 {
+    public static void main(String [] args) {
+        int a = 2;
+        int b = 8;
+        int result= calculation(a,b);
+        System.out.println(result);
+
+    }
+    static int calculation(int a, int b){
+        return (a + b) + (a * b);
+
+    }
+
+}
---
+++ src/main/java/com/rakovets/course/javabasics/practice/workbook/Task06.java
@@ -0,0 +1,17 @@
+package com.rakovets.course.javabasics.practice.workbook;
+
+public class Task06 {
+    public static void main(String[] args){
+        int a = 178;
+            System.out.println(checkSeven(a));
+    }
+
+    static boolean checkSeven(int a){
+        int resultOfCheck = a%10;
+        boolean result = false;
+        if ( resultOfCheck == 7){
+            result = true;
+        }
+        return result;
+    }
+}
